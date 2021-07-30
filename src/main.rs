3d21f5b4bdfe94a24e915073cdb17d1bd1862534
Éverton M. Vieira <everton.muvi@gmail.com>
2021-07-30 16:08:19 -0300

    prototype development

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Qinpel Server starting...");
+    HttpServer::new(|| App::new().service(ping))
+        .bind("0.0.0.0:5490")?
+        .run()
+        .await
 }
