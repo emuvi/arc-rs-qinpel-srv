c0f4f63befa77812406566235b3939b67b73c5ca
Éverton M. Vieira <everton.muvi@gmail.com>
2021-08-06 14:26:52 -0300

    prototype development

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
+use actix_files as fs;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
-#[get("/ping")]
-async fn ping() -> impl Responder {
-    HttpResponse::Ok().body("pong")
+fn init_run_folder() {
+    println!("init run folder..........");
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("QinpelSrv : 0.1.0")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Qinpel Server booting...");
+    init_run_folder();
     println!("Qinpel Server starting...");
-    HttpServer::new(|| App::new().service(ping))
-        .bind("0.0.0.0:5490")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(fs::Files::new("/run", "./run").index_file("index.html"))
+    })
+    .bind("0.0.0.0:5490")?
+    .run()
+    .await
 }
