73f29e9920070704a9e59e4c76e83114e4f2a386
Éverton M. Vieira <everton.muvi@gmail.com>
2021-11-22 18:00:48 -0300

    prototype development

--- src/lists.rs
+++ src/lists.rs
@@ -2,24 +2,32 @@ use actix_web::{HttpRequest, HttpResponse};
 
 use std::path::{Path, PathBuf};
 
-use super::data::Access;
-use super::guard;
-use super::SrvData;
-use super::SrvResult;
+use crate::data::Access;
+use crate::guard;
+use crate::SrvData;
+use crate::SrvError;
+use crate::SrvResult;
 
 pub fn list_app(req: &HttpRequest, srv_data: &SrvData) -> SrvResult {
 	let user = guard::get_user_or_err(req, srv_data)?;
-	if user.master {
-		return list_folder_dirs(Path::new("./run/app").to_owned());
-	}
+	let dirs = list_folder_dirs(Path::new("./run/app").to_owned())?;
 	let mut body = String::new();
-	for user_access in &user.access {
-		match user_access {
-			Access::APP { name } => {
-				body.push_str(name);
-				body.push_str("\n");
+	if user.master {
+		dirs.into_iter().for_each(|dir| {
+			body.push_str(&dir);
+			body.push_str("\n");
+		});
+	} else {
+		for user_access in &user.access {
+			match user_access {
+				Access::APP { name } => {
+					if dirs.contains(name) {
+						body.push_str(name);
+						body.push_str("\n");
+					}
+				}
+				_ => {}
 			}
-			_ => {}
 		}
 	}
 	Ok(HttpResponse::Ok().body(body))
@@ -27,37 +35,45 @@ pub fn list_app(req: &HttpRequest, srv_data: &SrvData) -> SrvResult {
 
 pub fn list_cmd(req: &HttpRequest, srv_data: &SrvData) -> SrvResult {
 	let user = guard::get_user_or_err(req, srv_data)?;
-	if user.master {
-		return list_folder_dirs(Path::new("./run/cmd").to_owned());
-	}
+	let dirs = list_folder_dirs(Path::new("./run/cmd").to_owned())?;
 	let mut body = String::new();
-	for user_access in &user.access {
-		match user_access {
-			Access::CMD { name, params: _ } => {
-				body.push_str(name);
-				body.push_str("\n");
+	if user.master {
+		dirs.into_iter().for_each(|dir| {
+			body.push_str(&dir);
+			body.push_str("\n");
+		});
+	} else {
+		for user_access in &user.access {
+			match user_access {
+				Access::CMD { name, params: _ } => {
+					if dirs.contains(name) {
+						body.push_str(name);
+						body.push_str("\n");
+					}
+				}
+				_ => {}
 			}
-			_ => {}
 		}
 	}
 	Ok(HttpResponse::Ok().body(body))
 }
 
-fn list_folder_dirs(folder: PathBuf) -> SrvResult {
-	let mut body = String::new();
-	for entry in folder.read_dir()? {
-		let entry = entry?;
-		let path = entry.path();
-		if path.is_dir() {
-			if let Some(name) = path.file_name() {
-				if let Some(name) = name.to_str() {
-					body.push_str(name);
-					body.push_str("\n");
+fn list_folder_dirs(folder: PathBuf) -> Result<Vec<String>, SrvError> {
+	let mut result = Vec::new();
+	if folder.exists() {
+		for entry in folder.read_dir()? {
+			let entry = entry?;
+			let path = entry.path();
+			if path.is_dir() {
+				if let Some(name) = path.file_name() {
+					if let Some(name) = name.to_str() {
+						result.push(String::from(name));
+					}
 				}
 			}
 		}
 	}
-	Ok(HttpResponse::Ok().body(body))
+	Ok(result)
 }
 
 pub fn list_dbs(req: &HttpRequest, srv_data: &SrvData) -> SrvResult {
