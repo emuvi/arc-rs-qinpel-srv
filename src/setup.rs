2b85fe04c99798bde2245d4414e40248581dbb2b
Éverton M. Vieira <everton.muvi@gmail.com>
2022-02-02 16:06:43 -0300

    prototype

--- src/setup.rs
+++ src/setup.rs
@@ -17,20 +17,53 @@ pub struct Head {
 
 impl Head {
     pub fn load(args: ArgMatches<'_>) -> Self {
-        let mut setup_host = DEFAULT_HOST;
+        let mut setup_debug = false;
+        let mut setup_verbose = false;
+        let mut setup_host = String::from(DEFAULT_HOST);
         let mut setup_port = DEFAULT_PORT;
-        let setup_debug = args.is_present("debug");
-        if setup_debug {
-            crate::DEBUG.store(true, Ordering::Relaxed);
+        let path = Path::new("setup.json");
+        if path.exists() {
+            let file = std::fs::File::open(path).expect("Setup file exists but could not be open.");
+            let setup_file: Value =
+                serde_json::from_reader(file).expect("Setup file exists but could not be parsed.");
+            match &setup_file["serverDebug"] {
+                Value::Bool(server_debug) => {
+                    setup_debug = *server_debug;
+                }
+                _ => {}
+            };
+            match &setup_file["serverVerbose"] {
+                Value::Bool(server_verbose) => {
+                    setup_verbose = *server_verbose;
+                }
+                _ => {}
+            };
+            match &setup_file["serverHost"] {
+                Value::String(server_host) => {
+                    setup_host = String::from(server_host);
+                }
+                _ => {}
+            };
+            match &setup_file["serverPort"] {
+                Value::Number(server_port) => {
+                    setup_port = server_port
+                        .as_u64()
+                        .expect("Could not parse the server port from setup file.");
+                }
+                _ => {}
+            };
         }
-        let setup_verbose = args.is_present("verbose");
-        if setup_verbose {
-            crate::VERBOSE.store(true, Ordering::Relaxed);
+        if args.is_present("debug") {
+            setup_debug = true;
+        }
+        if args.is_present("verbose") {
+            setup_verbose = true;
         }
         if args.is_present("host") {
-            setup_host = args
-                .value_of("host")
-                .expect("Could not read the host argument.");
+            setup_host = String::from(
+                args.value_of("host")
+                    .expect("Could not read the host argument."),
+            );
         }
         if args.is_present("port") {
             setup_port = args
@@ -39,35 +72,16 @@ impl Head {
                 .parse()
                 .expect("Could not parse the port argument.");
         }
-        let path = Path::new("setup.json");
-        let setup_value: Value = if !path.exists() {
-            Value::Null
-        } else {
-            let file = std::fs::File::open(path).expect("Setup file exists but could not be open.");
-            serde_json::from_reader(file).expect("Setup file exists but could not be parsed.")
-        };
-        if !args.is_present("host") {
-            let server_host = match &setup_value["serverHost"] {
-                Value::String(server_host) => &server_host,
-                Value::Null => DEFAULT_HOST,
-                _ => panic!("Wrong type value for the host in the setup file."),
-            };
-            setup_host = server_host;
+        if setup_debug {
+            crate::DEBUG.store(true, Ordering::Relaxed);
         }
-        if !args.is_present("port") {
-            let server_port = match &setup_value["serverPort"] {
-                Value::Number(server_port) => server_port
-                    .as_u64()
-                    .expect("Could not parse the server port from setup file."),
-                Value::Null => DEFAULT_PORT,
-                _ => panic!("Wrong type value for the port in the setup file."),
-            };
-            setup_port = server_port;
+        if setup_verbose {
+            crate::VERBOSE.store(true, Ordering::Relaxed);
         }
         Head {
             debug: setup_debug,
             verbose: setup_verbose,
-            host: String::from(setup_host),
+            host: setup_host,
             port: setup_port,
         }
     }
